edbe476a95d5021ba6841efd771ec72c12af4940
André Vinícius Roque Cavalcante <67342516+avroque@users.noreply.github.com>
2020-08-20 16:09:53 -0300

    Add files via upload

---
+++ SimulBezerra2019.m
@@ -0,0 +1,11 @@
+% 
+% Programa para reproduzir estudo de bentificação e homegenização biótica
+% Realizar simulações a partir de dados experimentais e bibliográficos
+% Posteior modelagem demografica multiespecies para analisa favorecimento
+% de especies não nativas em ambiente aquático com estiagens extremas
+% Comparar reservtório Tropical com regiao temperada
+%
+% Autor: André Cavalcante
+%
+% Inicio 13 de agosto de 2020
+% 
